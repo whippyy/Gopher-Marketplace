bf058a848cf204da185820b468ce971feeb62372
henryle <le000379@umn.edu>
2025-07-03 13:39:12 -0500

    changine firebase to use env variables

--- client/lib/firebase.ts
+++ client/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA3NJqx1EfvJ-0SG9TaBhGio4q-mOhbYnI",
-  authDomain: "gopher-marketplace-255c2.firebaseapp.com",
-  projectId: "gopher-marketplace-255c2",
-  storageBucket: "gopher-marketplace-255c2.firebasestorage.app",
-  messagingSenderId: "628980440330",
-  appId: "1:628980440330:web:d2e3ef8257e001dfa12ef3",
-  measurementId: "G-B3YDZ019G8"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
