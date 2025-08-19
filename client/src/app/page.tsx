bf699d6e97e2c691823f947e3da1778bfce664a4
henryle <le000379@umn.edu>
2025-08-19 14:26:19 -0500

    profile test

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -69,7 +69,7 @@ export default function Home() {
             
             <div className="flex space-x-4">
               <button
-                onClick={() => router.push('/profile/profile')}
+                onClick={() => router.push('/profile')}
                 className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 transition-colors"
               >
                 View Profile
