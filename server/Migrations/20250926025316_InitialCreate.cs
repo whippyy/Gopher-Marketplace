76a0f65a2a29a267e3765afff2265eed5cdaf687
henryle <le000379@umn.edu>
2025-09-25 21:53:21 -0500

    cache

---
+++ server/Migrations/20250926025316_InitialCreate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrls",
+                table: "Listings");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrls",
+                table: "Listings",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
